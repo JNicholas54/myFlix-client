cc3c809bca448ccf2b86811e4989a83c1a797268
JNicholas54 <102398723+JNicholas54@users.noreply.github.com>
2022-11-22 18:15:03 -0800

    Create main-view.jsx

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class MainView extends React.Component {
+    render() {
+        return (
+            <div className="main-view">
+                <div>A Few Good Men</div>
+                <div>My Cousin Vinny</div>
+                <div>Jurassic Park</div>
+            </div>
+        );
+    }
+}
