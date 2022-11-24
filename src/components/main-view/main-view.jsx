75a624bdebfd87f42d1b3a9708e77086431f0bfb
JNicholas54 <102398723+JNicholas54@users.noreply.github.com>
2022-11-23 20:12:52 -0800

    restructure main-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 
 export class MainView extends React.Component {
-    render() {
-        return (
-            <div className="main-view">
-                <div>A Few Good Men</div>
-                <div>My Cousin Vinny</div>
-                <div>Jurassic Park</div>
-            </div>
-        );
+    constructor() {
+        super();
+        this.state = {
+            movies: [
+                {_id: 1, Title: 'A Few Good Men', Description: 'desc1...', ImagePath: '...'},
+                {_id: 2, Title: 'My Cousin Vinny', Description: 'desc2...', ImagePath: '...'},
+                {_id: 3, Title: 'Jurassic Park', Description: 'desc3...', ImagePath: '...'},
+            ]
+        }
     }
-}
+
+    render() {
+        const { movies } = this.state;
+      
+        if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+      
+        return (
+          <div className="main-view">
+            {movies.map(movie => <div key={movie._id}>{movie.Title}</div>)}
+          </div>
+        );
+      }
+    }
+
