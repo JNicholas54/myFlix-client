5e5baa0d6c17c266a84729146c7b0e036b2cb8fe
JNicholas54 <102398723+JNicholas54@users.noreply.github.com>
2022-12-17 09:04:07 -0800

    updated spelling and spaces
    fixed spelling error, removed extra spacing

--- src/index.jsx
+++ src/index.jsx
@@ -6,13 +6,13 @@ import { MovieCard } from './components/movie-card/movie-card';
 
 import Container from 'react-bootstrap/Container';
 
+//the line below needs to come before the './index.scss' line
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 // Import statement ot indicate that you need to bundle './index.scss'
 import './index.scss';
 
-// Main component (will evwntually use all the others)
+// Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
     render() {
         return (
